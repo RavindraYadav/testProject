2cf03c8c639ef88d0428cd0e7ff080698ad81a9b
Ravindra1 <ravindray125@gmail.com>
2015-03-22 17:33:23 +0530

    chrome for class b

--- SampleAntProject/src/com/pack/two/TestB.java
+++ SampleAntProject/src/com/pack/two/TestB.java
@@ -1,16 +1,23 @@
 package com.pack.two;
 
+import java.io.File;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class TestB {
-	
+	WebDriver driver;
 	@BeforeClass
 	public void setUp() {
 		System.out.println("*** In class - Test B ***");
+		File f=new File("F:\\CloneProject\\NewTest\\SampleAntProject\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", f.getAbsolutePath());
+		driver=new ChromeDriver();
+		driver.get("https://google.com");
 	}
 
 	@Test
